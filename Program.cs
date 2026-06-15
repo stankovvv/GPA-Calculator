eadf304bbcd9bfb14bb7616edaff9cfe03212ed2
stankovvv <mykola.stankov07@gmail.com>
2026-06-15 19:39:35 +0300

    added some stuff i acsidentally reinited the repo lol

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+// init the grades
+int gradeA = 4;
+int gradeB = 3;
+
+// srore them 
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+// display the output
+//REMINDER FOR MYSELF $ work like jquory to insert some value in the string
+//@ to break some rules of string like new line and tab
+Console.WriteLine($"{course1Name} {course1Grade}");
+Console.WriteLine($"{course2Name} {course2Grade}");
+Console.WriteLine($"{course3Name} {course3Grade}");
+Console.WriteLine($"{course4Name} {course4Grade}");
+Console.WriteLine($"{course5Name} {course5Grade}");
+
+
+
